71b12da753fc3cf3776097cc6e0717326a397fc8
Long G <long.w.guan@gmail.com>
2023-09-13 16:21:24 -0700

    Dockerized react frontend and installed bootstrap

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="card" style={{width: "18rem"}}>
+      test
+    </div>
+  );
+}
+
+export default App;
