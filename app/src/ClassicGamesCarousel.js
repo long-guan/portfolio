b878b8e34290027affd86aa20e6fe168b7678ba7
Long G <long.w.guan@gmail.com>
2023-10-20 18:16:46 -0700

    Added features for battleship and lazy knight

--- app/src/ClassicGamesCarousel.js
+++ app/src/ClassicGamesCarousel.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import variance_tic_tac_toe from "./assets/variance_tic_tac_toe_compressed.mp4";
 import unbeatable_tic_tac_toe from "./assets/unbeatable_tic_tac_toe_compressed.mp4";
+import demo_lazy_knight from "./assets/demo_lazy_knight_compressed.mp4";
+import demo_battleship from "./assets/demo_battleship_compressed.mp4";
 
 function ClassicGamesCarousel() {
   const [index, setIndex] = useState(0);
@@ -18,7 +20,7 @@ function ClassicGamesCarousel() {
             src={unbeatable_tic_tac_toe}
             controls="controls"
             autoPlay={true}
-            width="500px"
+            width="auto"
             height="500px"
           ></video>
           <p className="carousel-label">
@@ -32,7 +34,7 @@ function ClassicGamesCarousel() {
           <video
             src={variance_tic_tac_toe}
             controls="controls"
-            width="500px"
+            width="auto"
             height="500px"
           ></video>
           <p className="carousel-label">
@@ -41,6 +43,39 @@ function ClassicGamesCarousel() {
             randomly. See example above where the computer's move randomizes
             between top-left, top-right, bottom-left, and bottom-right because
             all four moves result in the same minimax score (same outcome).
+          </p>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="d-flex flex-column align-items-center justify-contenter">
+          <video
+            src={demo_lazy_knight}
+            controls="controls"
+            width="auto"
+            height="500px"
+          ></video>
+          <p className="carousel-label">
+            Used breadth-first search algorithm to calculate the least number of
+            moves required for the knight to travel from one spot to another.
+            Combined setTimeout method and transform CSS property to dynamically
+            display the movement of the knight.
+          </p>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className="d-flex flex-column align-items-center justify-contenter">
+          <video
+            src={demo_battleship}
+            controls="controls"
+            width="auto"
+            height="500px"
+          ></video>
+          <p className="carousel-label">
+            Utilized a set of boolean variables to ensure that if the computer
+            lands a hit, it will continue attacking the adjacent coordinates
+            until the ship is sunken to imitate human player logic. See example
+            above where the computer targets the adjacent coordinates until ship
+            is sunken.
           </p>
         </div>
       </Carousel.Item>
